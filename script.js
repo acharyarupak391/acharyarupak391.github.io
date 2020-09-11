56fb562b4c2db115d35ec655f130ad8babf870d1
Rupak Acharya <acharyarupak391@gmail.com>
2020-09-11 15:46:51 +0545

    added sth

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  el = document.querySelector(".fa-angle-double-down")
+  var el = document.querySelector(".fa-angle-double-down")
   el.addEventListener("click", e => {
     window.scrollTo({top: 500, behavior: 'smooth'})
+  })
+
+  document.addEventListener("scroll", e => {
+    if(window.scrollY > 50) {
+      el.style["display"] = "none";
+    } else {
+      el.style["display"] = "block"
+    }
+  })
+
+  var unsplash = document.querySelector(".unsplash")
+  unsplash.addEventListener("click", e => {
+    window.open("https://unsplash.com/photos/aOC7TSLb1o8", "windowname")
   })
   
 })
