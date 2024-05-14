c9ea77f4a63099802431278d57f3997533ea0353
acharyarupak391 <acharyarupak391@gmail.com>
2024-05-14 10:01:24 +0545

    Updates

---
+++ _next/static/xvpn-Wee34lVv3NONUlJc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/3cbd70ff48da2307.css","static/chunks/pages/index-0f49eb27d29917ab.js"],"/_error":["static/chunks/pages/_error-68465c2b347cf9de.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
