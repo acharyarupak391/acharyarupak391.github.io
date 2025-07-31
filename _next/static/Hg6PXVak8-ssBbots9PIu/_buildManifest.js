3f39699a8a800da6f9a21d60a9527f464834c424
acharyarupak391 <acharyarupak391@gmail.com>
2025-07-31 19:24:16 +0545

    Updates

--- _next/static/Hg6PXVak8-ssBbots9PIu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/451-2d806a42ab268232.js","static/css/908f6f3b6cf1832c.css","static/chunks/pages/index-c1a0e07914daf12f.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
